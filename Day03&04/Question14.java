7c4ac8938fe70b113108c239f0cc0ed0213151d9
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-21 06:11:20 +0530

    Update Question14.java

--- Day03&04/Question14.java
+++ Day03&04/Question14.java
@@ -1,5 +1,18 @@
 package day0102;
-
+import java.util.Scanner;
 public class Question14 {
-
+public static void main(String[] args) {
+		System.out.println("Enter Number to Reverse it: ");
+		Scanner sc=new Scanner(System.in);
+	int num=sc.nextInt();
+	int n=0;
+	int ans=0;
+	System.out.println("Bfore Reversing Number: "+num);
+	while(!(num<=1)) {
+		n=num%10;
+		ans=ans*10+n;
+		num=num/10;
+	}
+	System.out.println("After Reversing Number: "+ans);
+	}
 }
